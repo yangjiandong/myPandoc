a548689292eed28e4f09323d76b7d7618723bf21
yangjiandong <youngjiandong@sina.cn>
2019-09-16 09:17:33 +0800

    update

--- demo/git-book/script/pdf.rb
+++ demo/git-book/script/pdf.rb
@@ -3,10 +3,10 @@ task :pdf => :html do
   prince = Prince.new()
   html_string = File.new("output/index.html").read
   prince.add_style_sheets 'layout/second.css', 'layout/mac_classic.css'
-  
+
   File.open('output/book.pdf', 'w') do |f|
     f.puts prince.pdf_from_string(html_string)
   end
-  
+
   `open output/book.pdf`
 end
